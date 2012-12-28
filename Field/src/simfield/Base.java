0e3954c40ae31badb497ebb7055fa5e2c494e4e3
betel <betelgeuse525@gmail.com>
2012-12-28 18:07:09 +0900

    Update:RandomWalking Object

--- Field/src/simfield/Base.java
+++ Field/src/simfield/Base.java
@@ -1,16 +1,20 @@
 package simfield;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 public class Base {
 
+	private int freq	=	100;	//更新頻度(ミリ秒)
 	private int width	=	800;	//ウィンドウのサイズ
 	private int height	=	600;
-	private int x		=	400;	//オブジェクトの初期位置
-	private int y		=	300;
+	private float x		=	400;	//オブジェクトの初期位置
+	private float y		=	300;
+	long lastFrameTime;
+	long lastTime;
 
 	public void start(){
 		//ウィンドウの生成
@@ -22,18 +26,48 @@ public class Base {
 			e.printStackTrace();
 			System.exit(0);
 		}
+
 		//初期化
 		initGL();
+		getDelta();
+		lastTime = getTime();
+		Life01 life1 = new Life01(x,y);	//ライフの配置
+		Life01 life2 = new Life01(x+50,y);
 
 		//メインループ
 		while(!Display.isCloseRequested()){
 
-			renderLife();
+			renderLife(life1);
+			renderLife(life2);
+
 			Display.update();	//オンスクリーンに反映
 			Display.sync(60);	//FPSを60に固定
 		}
 	}
 
+
+	//描画処理
+	void renderLife(Life01 life){
+		int delta = getDelta();
+
+		if((getTime()-lastTime) > freq){
+			life.updateLife(delta,width,height);
+			life.render();
+			lastTime += freq;	//speed秒経過したことを記録する
+		}
+	}
+	//正確な時刻を得る
+	public long getTime(){
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+	//デルタ時間を得る
+	public int getDelta(){
+		long	time	=	getTime();
+		int		delta	=	(int)(time - lastFrameTime);
+		lastFrameTime	=	time;
+
+		return delta;
+	}
 	//OpenGLの初期化
 	void initGL(){
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
@@ -41,10 +75,4 @@ public class Base {
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	}
-	//描画処理
-	void renderLife(){
-		Life01 life01 = new Life01(x,y);
-		life01.render();
-	}
-
 }
--- Field/src/simfield/Life01.java
+++ Field/src/simfield/Life01.java
@@ -16,19 +16,34 @@ public class Life01 {
 	//描画
 	public void render(){
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL11.glColor3f(0.1f, 0.4f, 0.1f);
+		GL11.glColor3f(0.8f, 0.1f, 0.1f);
 
 		GL11.glPushMatrix();
 			GL11.glBegin(GL11.GL_QUADS);
 				GL11.glVertex2f(x-10, y-10);
-				GL11.glVertex2f(x+10, y-20);
+				GL11.glVertex2f(x+10, y-10);
 				GL11.glVertex2f(x+10, y+10);
 				GL11.glVertex2f(x-10, y+10);
 			GL11.glEnd();
 		GL11.glPopMatrix();
 	}
-	//ライフの向かう方向（0,1,2,3の4方向）
+	//ライフの向かう方向（1,2,3,4の4方向。0は移動しない）
 	public int getDirection(){
-		return (int)(4*Math.random());
+		return (int)(5*Math.random());
+	}
+	//移動（Baseクラスからデルタ時間を取得）
+	public void updateLife(int delta, int width, int height){
+		int d = getDirection();
+
+		if(d==0) return;
+		if(d==1) x -= 0.35f*delta;
+		if(d==2) y += 0.35f*delta;
+		if(d==3) x += 0.35f*delta;
+		if(d==4) y -= 0.35f*delta;
+
+		if(x<0) x=0;
+		if(x>width)  x=width;
+		if(y<0) y=0;
+		if(y>height) y=height;
 	}
 }
